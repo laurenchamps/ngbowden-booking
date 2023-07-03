3655285b4a6d202973d1428f39af241ebd705b69
Lauren Champs <laurenkchamps@gmail.com>
2023-07-03 12:32:57 +0930

    Add logic for login route

--- app.py
+++ app.py
@@ -52,23 +52,24 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
+
+        # Ensure email was submitted
+        if not request.form.get("email"):
+            return apology("Must provide email", 403)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("Must provide password", 403)
 
         # Query database for username
-        # rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute("SELECT * FROM users WHERE email = ?", request.form.get("email"))
 
         # Ensure username exists and password is correct
-        # if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("check_password_hash")):
-        #     return apology("invalid username and/or password", 403)
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("Invalid username and/or password", 403)
 
         # Remember which user has logged in
-        # session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0]["id"]
 
         # Redirect user to homepage
         return redirect("/")
@@ -137,8 +138,8 @@ def signup():
 
         print(id)
 
-        # Log user in *needs to be updated to index once built*
-        return render_template("login.html")
+        # # TODO Log user in *needs to be updated to index once built
+        # return render_template("login.html")
 
     else:
         return render_template("signup.html")
